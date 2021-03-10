b4a900a16412aebbd21b300194a5087b55737471
Jeffrey Alvarez <kuritsu@gmail.com>
2021-03-10 08:40:05 -0600

    Improve worker role notice message.

--- cookbooks/pozoledf-k8s-node/recipes/worker.rb
+++ cookbooks/pozoledf-k8s-node/recipes/worker.rb
@@ -1,3 +1,14 @@
 # worker recipe
 
 include_recipe '::common'
+
+file '/tmp/install-chef-client-notice.txt' do
+  content <<-EOM
+===> IMPORTANT: Check the last line of the /var/lib/kubelet/kubeinit.log file
+on the Kubernetes control plane node, and run the command contained there
+to add this node to the cluster.
+EOM
+  user  'root'
+  group 'root'
+  mode  '0644'
+end
