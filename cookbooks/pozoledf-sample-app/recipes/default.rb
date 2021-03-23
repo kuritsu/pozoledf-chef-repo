08fe1f9ff15c1a0617e88cdbe791fb1f07890581
Jeffrey Alvarez <kuritsu@gmail.com>
2021-03-23 16:15:13 -0600

    Fix builder_url.

--- cookbooks/pozoledf-sample-app/recipes/default.rb
+++ cookbooks/pozoledf-sample-app/recipes/default.rb
@@ -7,9 +7,11 @@
 include_recipe 'pozoledf-habitat::default'
 
 automate_info = data_bag_item('automate', 'info')
+builder_info = data_bag_item('builder', 'info')
 
 hab_service "#{automate_info['org']}/pozoledf-sample-app" do
   channel  node['chef_environment']
+  bldr_url builder_info['builder_url']
   strategy 'at-once'
   topology 'standalone'
   update_condition 'track-channel'
