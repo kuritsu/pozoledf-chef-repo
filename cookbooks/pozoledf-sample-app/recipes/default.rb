ef26e1871efbf3fe8a593fb84f5adc4a936a4c31
Jeffrey Alvarez <kuritsu@gmail.com>
2021-03-23 15:04:03 -0600

    Properly configure hab service.

--- cookbooks/pozoledf-sample-app/recipes/default.rb
+++ cookbooks/pozoledf-sample-app/recipes/default.rb
@@ -9,6 +9,8 @@ include_recipe 'pozoledf-habitat::default'
 builder_info = data_bag_item('builder', 'info')
 
 hab_service "#{builder_info['org']}/pozoledf-sample-app" do
+  channel  node['chef_environment']
   strategy 'at-once'
   topology 'standalone'
+  update_condition 'latest'
 end
