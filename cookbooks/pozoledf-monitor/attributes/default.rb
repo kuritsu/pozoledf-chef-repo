4511bdf87b89f112a7e6fec8b7b0ce217232e98c
Jeffrey Alvarez <kuritsu@gmail.com>
2021-04-01 22:02:12 -0600

    Use same shasum of rhel for amazon.

--- cookbooks/pozoledf-monitor/attributes/default.rb
+++ cookbooks/pozoledf-monitor/attributes/default.rb
@@ -2,7 +2,4 @@
 default['elasticsearch']['version'] = '7.12.0'
 default['elasticsearch']['download_url'] = 'https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.12.0-linux-x86_64.tar.gz'
 
-#<> Kibana7 exact version
-default['kibana']['kibana5_version'] = default['elasticsearch']['version']
-default['kibana']['kibana5_checksum'] = 'dbc1cb0ff6d0ddda687bde4ecb69b5e7da80e5a207398929b9bbe4e87e962d81'
-default['kibana']['kibana5_url'] = "https://artifacts.elastic.co/downloads/kibana/kibana-#{node['kibana']['kibana5_version']}-linux-x86_64.tar.gz"
+default['influxdb']['shasums']['amazon'] = node['influxdb']['shasums']['rhel']
