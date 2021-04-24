16db2bb57fcef1e6f123282eb78cca60590af672
Jeffrey Alvarez <kuritsu@gmail.com>
2021-04-24 13:02:05 -0500

    Update ES config.

--- cookbooks/pozoledf-monitor/recipes/elasticsearch.rb
+++ cookbooks/pozoledf-monitor/recipes/elasticsearch.rb
@@ -10,6 +10,14 @@ elasticsearch_install 'elasticsearch' do
 end
 
 elasticsearch_configure 'elasticsearch' do
+  configuration {
+    'cluster.name' => node['name'],
+    'node.name' => node['name'],
+    'http.port' => 9200,
+    'network.host' => '0.0.0.0',
+    'cluster.initial_master_nodes' => [node['name']]
+  }
+
   jvm_options %w(
     -Xms994m
     -Xmx994m
