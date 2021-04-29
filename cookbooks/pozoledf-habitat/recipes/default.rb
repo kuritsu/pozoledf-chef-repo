8e991b0600e974687702297660a1ee16fc6e7b17
Jeffrey Alvarez <kuritsu@gmail.com>
2021-04-29 18:10:52 -0500

    Install hab sup packages before sup runs.

--- cookbooks/pozoledf-habitat/recipes/default.rb
+++ cookbooks/pozoledf-habitat/recipes/default.rb
@@ -18,6 +18,30 @@ file '/hab/cache/ssl/automate.pem' do
   group 'root'
 end
 
+hab_package 'core/hab-sup' do
+  version '1.6.288'
+done
+
+hab_package 'core/hab-launcher' do
+  version '1.6.288'
+done
+
+hab_sup 'default' do
+  license                   'accept'
+  action                    :run
+  org                       automate_info['org']
+  hab_channel               node['chef_environment']
+  bldr_url                  builder_info['builder_url']
+  event_stream_application  node['name']
+  event_stream_environment  node['chef_environment']
+  event_stream_site         node['chef_environment']
+  event_stream_url          automate_info['stream_url']
+  event_stream_token        automate_info['stream_token']
+  event_stream_cert         '/hab/cache/ssl/automate.pem'
+  sup_version               '1.6.288'
+  toml_config               true
+end
+
 hab_sup 'default' do
   license                   'accept'
   action                    :run
