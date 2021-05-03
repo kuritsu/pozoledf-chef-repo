9d29b0d2b9980106ed9596c70bc110c14e1c2a79
Jeffrey Alvarez <kuritsu@gmail.com>
2021-05-03 17:56:38 -0500

    Use install type file for telegraf.

--- cookbooks/pozoledf-telegraf/attributes/default.rb
+++ cookbooks/pozoledf-telegraf/attributes/default.rb
@@ -1,3 +1,5 @@
+default['telegraf']['version'] = '1.18.0'
+default['telegraf']['install_type'] = 'file'
 default['telegraf']['config']['global_tags']['env'] = node['chef_environment']
 default['telegraf']['config']['global_tags']['name'] = node['name']
 default['telegraf']['config']['agent']['logfile'] = '/var/log/telegraf/telegraf.log'
