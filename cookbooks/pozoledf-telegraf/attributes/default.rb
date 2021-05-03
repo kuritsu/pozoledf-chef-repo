7d928d889c7a399cd08800bc668c9f021a5c972c
Jeffrey Alvarez <kuritsu@gmail.com>
2021-05-03 18:07:13 -0500

    Update shasum.

--- cookbooks/pozoledf-telegraf/attributes/default.rb
+++ cookbooks/pozoledf-telegraf/attributes/default.rb
@@ -1,5 +1,6 @@
 default['telegraf']['version'] = '1.18.2-1'
 default['telegraf']['include_repository'] = false
+default['telegraf']['shasums']['rhel'] = 'e568a22b9d35de89997263af7565ec6044ff91c92b5fbe54ad42e94b13594512'
 default['telegraf']['install_type'] = 'file'
 default['telegraf']['config']['global_tags']['env'] = node['chef_environment']
 default['telegraf']['config']['global_tags']['name'] = node['name']
